e5520d2887db50636d6b34b89293232703573102
William L <w-lindvall@outlook.com>
2019-04-24 15:59:48 -0700

    feat: sketch reactivity system

---
+++ src/index.js
@@ -0,0 +1,44 @@
+/* PokeJS
+ *********/
+
+// Usage:
+// 1. run init with your data obj to set up appropriate getters/setters on vars
+// 2. create a reactive variable by passing its assignment in a fn to watch
+
+let activeFn
+function Dependency () {
+  const updaters = []
+
+  const addUpdater = () =>
+    (activeFn && !updaters.includes(activeFn)) &&
+      updaters.push(activeFn)
+
+  const runUpdaters = () =>
+    updaters.forEach(fn => fn())
+
+  return { addUpdater, runUpdaters }
+}
+
+export function init (data) {
+  Object.keys(data).forEach(key => {
+    const dependency = new Dependency()
+
+    let value = data[key]
+    Object.defineProperty(data, key, {
+      get () {
+        dependency.addUpdater()
+        return value
+      },
+      set (newValue) {
+        value = newValue
+        dependency.runUpdaters()
+      }
+    })
+  })
+}
+
+export function watch (fn) {
+  activeFn = fn
+  fn()
+  activeFn = null
+}
